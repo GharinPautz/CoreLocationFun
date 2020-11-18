977f184b5dfbe3e34e369592c3b9fd1704cc513f
Gharin Pautz <gharinpautz@gmail.com>
2020-11-18 11:43:16 -0800

    Initial Commit

---
+++ CoreLocationFun/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreLocationFun
+//
+//  Created by Gharin Pautz on 11/18/20.
+//  Copyright © 2020 Gharin Pautz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
